1f5cdc827d1d11e0073ae17ba92304622109f8b5
Bryan Vu <bryanvu@me.com>
2025-07-21 16:52:48 -0500

    updated server tests.

--- server/createApp.js
+++ server/createApp.js
@@ -1,11 +1,14 @@
 const express = require("express");
 
-function createApp({ movieController, tvController }) {
+function createApp({ movieController, tvController, searchController }) {
   const app = express();
   app.use(express.json());
 
   app.get("/api/movies/trending", movieController.getTrendingMovies);
+  app.get("/api/movies/genre", movieController.getMoviesByGenre);
   app.get("/api/tv/trending", tvController.getTrendingTV);
+  app.get("/api/tv/genre", tvController.getTvByGenre);
+  app.get("/api/search", searchController.searchMulti);
 
   return app;
 }
